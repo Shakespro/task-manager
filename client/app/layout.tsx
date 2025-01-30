082339b6c6bdcf5419a4827b4c39542f12e2aaa2
Shakespro <135011204+Shakespro@users.noreply.github.com>
2025-01-30 09:14:54 +0200

    chart implementation complete

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,22 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import UserProvider from "@/providers/UserProvider";
+import { Inter } from "next/font/google";
 import MiniSidebar from "./Components/MiniSidebar/MiniSidebar";
 import Header from "./Components/Header/Header";
-import "./globals.css";
-import { Main } from "next/document";
 import MainContentLayout from "@/providers/MainContentLayout";
 import SidebarProvider from "@/providers/SidebarProvider";
 import MainLayout from "@/providers/MainLayout";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
   subsets: ["latin"],
 });
 
@@ -32,28 +25,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" 
-      integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" 
-      crossOrigin="anonymous" 
-      referrerPolicy="no-referrer" 
-      />
-      </head>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-         <UserProvider>
-         <Toaster position= "top-center" />
+      <body className={inter.className}>
+        <UserProvider>
+          <Toaster position="top-center" />
 
-         <div className='h-full flex overflow-hidden'>
-          <MiniSidebar  />
-          <div className="flex-1 flex flex-col">
-            <Header />
+          <div className="h-full flex overflow-hidden">
+            <MiniSidebar />
+            <div className="flex-1 flex flex-col">
+              <Header />
+              <MainContentLayout>
+                <MainLayout>{children}</MainLayout>
+                <SidebarProvider />
+              </MainContentLayout>
+            </div>
           </div>
-          <MainContentLayout>
-            <MainLayout>{children}</MainLayout>
-            <SidebarProvider />
-            </MainContentLayout>
-         </div>
-      </UserProvider>
+        </UserProvider>
       </body>
     </html>
   );
